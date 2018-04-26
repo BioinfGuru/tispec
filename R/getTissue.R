0d8b31ca3be6ee460401a2a8a1795596a5def912
Kenneth Condon <roonysgalbi@gmail.com>
2018-04-26 15:27:07 +0100

    added score column to getTissue function

--- R/getTissue.R
+++ R/getTissue.R
@@ -3,7 +3,10 @@
 #' @description 
 #' This function takes a user defined tissue and extracts the ensembl 
 #' annotations, normalised expression values, tau values, and tau expression
-#' fractions of all genes in that tissue.
+#' fractions of all genes in that tissue. Each gene's score is the sum of it's 
+#' tau expression fraction value and it's 0-1 ranged normalised expression 
+#' value. The gene with the greatest expression and highest specificity 
+#' recieves the highest score.
 #' @param x tissue of interest (case sensitive)
 #' @param y output from quantNorm()
 #' @param z output from getMart()
@@ -22,8 +25,15 @@
 getTissue <- function(x, y, z){
     y <- y[order(rownames(y)),]
     z <- z[order(rownames(z)),]
-    temp <- z[,c("external_gene_name", "gene_biotype", "tau", x)]
-    temp <- cbind(temp, y[x])
-    colnames(temp)[c(4,5)] <- c('frac', 'qn')
-    temp[,c(1,2,5,3,4)]
+    df <- z[,c("external_gene_name", "gene_biotype", "tau", x)]
+    df <- cbind(df, y[x])
+    colnames(df)[c(4,5)] <- c('frac', 'qn')
+    df[,c(1,2,5,3,4)]
+    
+    # get score
+    range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+    df$qnRanged <- range01(df$qn)
+    df$score <- round(df$frac + df$qnRanged, digits = 3)
+    df$qnRanged <- NULL
+    return(df)
 }
